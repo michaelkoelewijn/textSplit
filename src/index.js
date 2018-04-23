b1ca5a018fcdff5c896450b8d36a425f38a71330
Michael Koelewijn <michael.koelewijn@wearejust.com>
2018-04-23 17:37:28 +0200

    Rebuild package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../dist/style.css';
+import '../src/style.css';
 const $ = require('jquery');
 
 export function init(options) {
@@ -9,63 +9,48 @@ class TextSplit {
     constructor(options) {
         this.name = options.name;
         this.item = options.element;
-        this.after = options.after;
-        this.smoothen = options.smoothen;
+        this.offset = options.offset;
 
+        this.animSpeed = options.speed; //in MS
+        this.animOptions = {
+            'transform': 'translate3d(0,0,0)',
+            'transition-timing-function': options.easing
+        }
         this.text = this.item.text();
         this.window = $(window);
-        
+        this.init();
         this.resize();
         this.window.on('resize', this.resize.bind(this));
         this.window.on('scroll', this.scroll.bind(this));
     }
 
     resize() {
-        this.windowWidth = this.window.outerWidth();
-        this.windowHeight = this.window.outerHeight();
+        this.position = this.item.offset().top;
         this.height = this.item.outerHeight();
-
-        this.position = this.item.offset();
-
-        this.init();
-        this.scroll();
+        this.windowHeight = this.window.outerHeight();
     }
 
     scroll() {
-        let viewportHeight = this.windowHeight;
-        let scrollTop = $(window).scrollTop();
-        let elementHeight = this.height;
-        let elementOffsetTop = this.position.top;
-        let percentage, y;
-    
-        y = 0;
-        if(scrollTop >= this.after) {
-            percentage = Math.min(100, (((scrollTop - this.after) / this.smoothen)))
-            y = ((elementHeight / 2) / 100) * percentage;
+        const rect = this.item[0].getBoundingClientRect();
+        if(rect.bottom > 0 && (rect.top + this.height + this.offset) < window.innerHeight) {
+            this.textUpper.css(this.animOptions);
+            this.textLower.css(this.animOptions);
         }
-        
-        this.split(y);
-        
-    }
-
-    split(y) {
-        this.textUpper.css({ transform: `translate3d(0,${y}px,0)` });
-        this.textLower.css({ transform: `translate3d(0,${y*-1}px,0)` });
     }
 
     init()  {
-
-        this.item.css({'position': 'relative'})
+        this.item.addClass('has-loaded');
+        this.item.css({ 'position': 'relative' })
 
         let textOriginal = $('<span class="js-textsplit-original" />')
         .text(this.text);
         
         let textSplitUpper = $('<span class="js-textsplit-half js-textsplit-half-upper" />')
-                            .html($(`<span class="js-textsplit-upper" style="transform: translate3d(0,0,0)" />`)
+                            .html($(`<span class="js-textsplit-upper" style="transform: translate3d(0,100%,0);transition: ${this.animSpeed}ms;" />`)
                             .text(this.text));
     
         let textSplitLower = $('<span class="js-textsplit-half js-textsplit-half-lower" />')
-                            .html($(`<span class="js-textsplit-lower" style="transform: translate3d(0,0,0)" />`)
+                            .html($(`<span class="js-textsplit-lower" style="transform: translate3d(0,-100%,0);transition: ${this.animSpeed}ms;" />`)
                             .text(this.text));
         
         this.item.html('').append(textOriginal, textSplitUpper, textSplitLower)
